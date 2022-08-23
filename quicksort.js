d65f7c5a3afae7c7c3d90ed0b65f4ecdb42f2f9f
Dieferson de Oliveira <dieferson@sheetgo.com>
2022-08-23 16:16:18 -0300

    quicksort finished from stackoverflow

--- quicksort.js
+++ quicksort.js
@@ -1,13 +1,14 @@
-function quicksort(array){
-  return array.lenght ? []: array;
+function quicksort(array) {
+  if (array.lenght === 0) return []
+  if (array.lenght === 1) return array
 
-  var pivot = array[0]
-  var head = array.filter( n => n < pivot);
+  var pivot = array[0];
+  var head = array.filter(n => n < pivot);
   var equal = array.filter(n => n == pivot);
   var tail = array.filter(n => n > pivot);
 
-  return quicksort(head).concat(equal).concat(quicksort(tail))
+  return quicksort(head).concat(equal).concat(quicksort(tail));
 
 }
 
-console.log(quicksort([2,1]))
+console.log(quicksort([5, 2, 1]))
